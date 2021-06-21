9243430c65c5195905e2366123781f2e22381b2f
JoGusTor <gustavotorresacosta@gmail.com>
2021-06-21 10:19:57 -0500

    Se añade todo el workspace

---
+++ holaMundo/src/main/java/net/prueba/holaMundo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package net.prueba.holaMundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String inicio() {
+		return "Hola Mundo";
+		// TODO Auto-generated method stub
+
+	}
+}
